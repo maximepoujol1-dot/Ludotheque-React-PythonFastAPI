c720f527a35c9cb6bbec7bf0a599d63667c5019c
maximepoujol1-dot <maximepoujol1@gmail.com>
2026-09-25 14:07:16 +0200

    config correction

--- api/core/config.py
+++ api/core/config.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
 CORS_ORIGINS = os.environ.get("CORS_ORIGINS")
 
