5f6f79e998fb7e762f219cb3caac96ce1a9222ec
maximepoujol1-dot <maximepoujol1@gmail.com>
2026-09-25 14:20:42 +0200

    main, requierement update

--- api/main.py
+++ api/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
-from routers import auth_routes, items_routes, collection_routes
-from db.session import Base, engine
-from core import error
+from .models.user import Users
+from .models.item import Items
+from .models.collection import Collection
+from .routers import auth_routes, items_routes, collection_routes
+from .db.session import create_table
+from .core import errors
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    print("created")
+    create_table()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_routes.router)
 app.include_router(items_routes.router)
 app.include_router(collection_routes.router)
-app.include_router(error.router)
+app.include_router(errors.router)
 
 @app.get("/")
-async def read_root():
-    return {"message": "Bonjour"}
+async def root():
+    return {"root": "hello world"}
 
-Base.metadata.create_all(engine)    
+@app.get("/health")
+async def health_check():
+    return {"status": "running..."}
+
